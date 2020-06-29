88ff66db387c7e3d236bc1a2695789453bd3085e
Kalyan Gurugubelli <kalyangurugubelli@Kalyans-MacBook-Pro.local>
2020-06-29 11:21:20 -0400

    https://github.com/kalyan88/React-interface.git

---
+++ src/components/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments';
+import SearchAppointments from './SearchAppointments';
+import { without } from 'lodash';
+
+
+class App extends Component {
+
+    constructor() {
+      super();
+      this.state = {
+        formDisplay: false,
+        myAppointments:[],
+        orderBy: 'petName',
+        orderDir: 'asc',
+        lastIndex: 0
+      };
+      this.deleteAppoitnment = this.deleteAppoitnment.bind(this);
+      this.toggleForm = this.toggleForm.bind(this);
+      this.addAppointment = this.addAppointment.bind(this);
+    }
+
+    toggleForm() {
+      this.setState({
+        formDisplay: !this.state.formDisplay
+      })
+    }
+
+    addAppointment(apt) {
+      let tempApts = this.state.myAppointments;
+      apt.aptId = this.state.lastIndex;
+      tempApts.unshift(apt); //unshift is JS function will push item to an array and place beggining of the array
+      this.setState({
+        myAppointments: tempApts,
+        lastIndex:this.state.lastIndex+1 //incrementing the index after adding item
+      });
+    }
+
+    deleteAppoitnment(apt) {
+      let tempApts = this.state.myAppointments;
+      tempApts = without(tempApts, apt)
+
+      this.setState({
+        myAppointments: tempApts
+      })
+    }
+
+    componentDidMount() {
+      
+      fetch('./data.json')
+            .then(response =>response.json())
+            .then(result => {
+                const appointments = result.map(item => {
+                  item.aptId = this.state.lastIndex;
+                  this.setState({lastIndex:  this.state.lastIndex+1}) //this will increment the lastIndex inside constructor()
+                    return item;
+                  })
+                  this.setState({
+                    myAppointments: appointments
+                  });
+            });
+    }
+
+    render() {
+      let order;
+      let filteredApts = this.state.myAppointments;
+      if(this.state.orderDir === 'asc') {
+        order =1; // to show Ascending order
+      } else {
+        order = -1;// to show decending order
+      }
+      filteredApts.sort((a,b) => {
+        if(a[this.state.orderBy].toLowerCase() <
+                b[this.state.orderBy].toLowerCase()){
+                  return -1* order;
+                } else{
+                  return 1* order
+                }
+      })
+      return (
+        <main className="page bg-white" id="petratings">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 bg-white">
+                  <div className="container">
+                    <AddAppointments 
+                      formDisplay={this.state.formDisplay}
+                      toggleForm={this.toggleForm}
+                      addAppointment={this.addAppointment}/>
+                    <SearchAppointments />
+                    <ListAppointments appointments={filteredApts}
+                    deleteAppoitnment={this.deleteAppoitnment}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+      );
+    }
+}
+
+export default App;
